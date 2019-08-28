a72bb3d00bb3b931a393f25d94e563af1453980c
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2019-08-28 13:33:16 +0200

    made the jest tests running

--- jira/bookingSummarizer.test.js
+++ jira/bookingSummarizer.test.js
@@ -1,4 +1,8 @@
+const {blubbaDeleteMe} = require("./bookingSummarizer.js")
+
 describe("Some simple example test", () => {
+
     it('should work', () => {
+        expect(blubbaDeleteMe()).toBe("whoooooot######");
     });
 });
