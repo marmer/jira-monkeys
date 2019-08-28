e73cef69fbe01ec59eac660780f56a183564dea8
MarMer <1055445+marmer@users.noreply.github.com>
2019-08-28 14:07:24 +0200

    not needed downloadURL removed

--- jira/bookingSummarizer.js
+++ jira/bookingSummarizer.js
@@ -9,7 +9,6 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
 // @updateURL    https://raw.githubusercontent.com/marmer/Tampermonkeys/master/jira/bookingSummarizer.js
-// @downloadURL  https://raw.githubusercontent.com/marmer/Tampermonkeys/master/jira/bookingSummarizer.js
 // ==/UserScript==
 
 (function () {
