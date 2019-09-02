db4d700bfd9650df5ee45579410e184a486ce62c
MarMer <1055445+marmer@users.noreply.github.com>
2019-09-02 19:30:27 +0200

    some spiky tries to recognize whether our custom elements have been gone

--- jira/src/service/App.test.ts
+++ jira/src/service/App.test.ts
@@ -2,20 +2,12 @@ import WorklogSummarizer from "./WorklogSummarizer";
 import App from "./App";
 
 describe("a running app", () => {
-    let registerMock: jest.Mock<any, any>;
-
-    beforeEach(cb => {
-        registerMock = jest.fn().mockImplementationOnce(args => {
-            // TODO: marmer 02.09.2019 Get out why this is not called oO
-            console.log("why is this not called oO")
-        });
-        WorklogSummarizer.prototype.register = registerMock;
+    beforeEach(() => {
+        WorklogSummarizer.prototype.register = jest.fn();
     });
 
     it('should register a worklog Summarizer', () => {
-        // expect(registerMock).toBeCalled();
         App.run();
-        // expect(registerMock).toBeCalled();
-        expect(registerMock).toHaveBeenCalled();
+        expect(WorklogSummarizer.prototype.register).toHaveBeenCalled();
     });
 });
--- jira/src/service/App.ts
+++ jira/src/service/App.ts
@@ -2,8 +2,6 @@ import WorklogSummarizer from "./WorklogSummarizer";
 
 export default class App {
     static run() {
-        const worklogSummarizer = new WorklogSummarizer();
-        const register = worklogSummarizer.register;
-        register();
+        new WorklogSummarizer().register();
     }
 }
