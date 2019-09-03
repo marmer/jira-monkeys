78a1a2f7f6858bc7ef2bc7de2911de8f94a401ab
MarMer <1055445+marmer@users.noreply.github.com>
2019-09-03 18:41:53 +0200

    some workflow idea

--- jira/src/service/App.ts
+++ jira/src/service/App.ts
@@ -3,5 +3,9 @@ import JiraSpike from "./JiraSpike";
 export default class App {
     static run() {
         new JiraSpike().register();
+
+        // Element to register are created
+        // Element have to be registered (so that they stay even if Jira tries to remove it as long as the parent element is not gone)
+        // Element maintains state by itself!
     }
 }
