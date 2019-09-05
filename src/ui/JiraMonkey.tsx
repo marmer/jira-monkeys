7b19a757fa281a96a792a88ed30f5890063a058f
MarMer <1055445+marmer@users.noreply.github.com>
2019-09-05 18:36:21 +0200

    a little refactoring and a sketch of a test

--- src/ui/JiraMonkey.tsx
+++ src/ui/JiraMonkey.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import WorklogSummarizerView from "./WorklogSummarizerView";
 
 // TODO: marmer 05.09.2019 make the component refres/reload if the "site changes" (the site content gets replaced by jira)
-export default (): React.ReactElement => <div>
-    Curtent pate title is: {document.title}
-    Version: {process.env.NODE_ENV}
-</div>
+
+export default (): React.ReactElement => {
+    return <div>
+        <WorklogSummarizerView/>
+    </div>
+}
---
+++ src/ui/WorklogSummarizerView.tsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+interface WorklogSummarizerViewState {
+
+}
+
+export interface WorklogSummarizerViewProps {
+
+}
+
+export default class WorklogSummarizerView extends Component<WorklogSummarizerViewProps, WorklogSummarizerViewState> {
+
+    constructor(props: Readonly<WorklogSummarizerViewProps>) {
+        super(props);
+    }
+
+    render(): React.ReactElement {
+        return <div>
+
+        </div>;
+    }
+
+}
