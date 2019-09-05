1b21572711c6c3f481172a2f4b8000534fafd4b2
MarMer <1055445+marmer@users.noreply.github.com>
2019-09-05 22:05:23 +0200

    Worklogs can be shown summarized now within the UI

--- src/ui/WorklogSummarizerView.tsx
+++ src/ui/WorklogSummarizerView.tsx
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
+import WorklogService, {Worklog} from "../core/WorklogService";
+import jiraFormat from "../core/jiraFormat";
 
 interface WorklogSummarizerViewState {
-
+    loadingState: "LOADING" | "DONE" | "ERROR",
+    worklogs: Worklog[]
 }
+
 
 export interface WorklogSummarizerViewProps {
 
@@ -12,12 +16,40 @@ export default class WorklogSummarizerView extends Component<WorklogSummarizerVi
 
     constructor(props: Readonly<WorklogSummarizerViewProps>) {
         super(props);
+        this.state = {
+            loadingState: "LOADING",
+            worklogs: []
+        };
+
+        this.loadSummedBookings();
     }
 
     render(): React.ReactElement {
         return <div>
-            Some Summarizing Content :D
+            <h1>Worklogs summarized per User</h1>
+            {this.state.loadingState === "ERROR" && "Error"}
+            {this.state.loadingState === "LOADING" && "Loading"}
+            {this.state.loadingState === "DONE" && this.state.worklogs.map(worklog =>
+                <div key={worklog.author.displayName}>
+                    {worklog.author.displayName} => {jiraFormat(worklog.timeSpentInMinutes)}
+                </div>)}
         </div>;
+    }
+
+    private loadSummedBookings() {
+        this.setState({
+            loadingState: "LOADING",
+            worklogs: []
+        });
+        WorklogService.getSummedWorklogsByUser()
+            .then(worklogs => this.setState({
+                loadingState: "DONE",
+                worklogs: worklogs
+            }))
+            .catch(reason => {
+                this.setState({loadingState: "ERROR"})
+                return console.error(reason);
+            })
     }
 
 }
