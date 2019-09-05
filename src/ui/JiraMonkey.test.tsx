b7c28afa5d2aca0e03637522182aea939f852f96
marmer <Mariano.Mertinat@gmail.com>
2019-09-05 18:56:29 +0200

    - simple react unittest

--- src/ui/JiraMonkey.test.tsx
+++ src/ui/JiraMonkey.test.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import JiraMonkey from "./JiraMonkey";
+import {configure, shallow} from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import WorklogSummarizerView from "./WorklogSummarizerView";
+
+configure({adapter: new Adapter()});
 
 describe("JiraMonkey", () => {
 
     it('should render WorklogSummarizerView', () => {
-        // const jiraMonkey = mount(<JiraMonkey/>);
-        // jiraMonkey.find("WorklogSummarizerView").exists();
-        fail("Yeah, a classic fail")
+        const jiraMonkey = shallow(<JiraMonkey/>);
+        expect(jiraMonkey.contains(<WorklogSummarizerView/>)).toBe(true);
     });
 });
