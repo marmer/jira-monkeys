f0fc83d4030f6bb0434d491b88104b69c38e27b1
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2019-09-05 23:28:44 +0200

    some simple "Styling"

--- src/ui/JiraMonkey.tsx
+++ src/ui/JiraMonkey.tsx
@@ -1,25 +1,17 @@
 import React, {useState} from 'react';
 import WorklogSummarizerView from "./WorklogSummarizerView";
-
-// TODO: marmer 05.09.2019 make the component refres/reload if the "site changes" (the site content gets replaced by jira)
-
-
+import "./JiraMonkey.css"
+// TODO: marmer 05.09.2019 make the component refresh/reload if the "site changes" (the site content gets replaced by jira)
 export default (): React.ReactElement => {
     const [state, setState] = useState({
         toolsVisible: false
     });
 
     return <div className="aui-header aui-dropdown2-trigger-group">
-        <label>
-            <input type="checkbox"
-                   className="aui-button toggle-title"
-                   checked={state.toolsVisible}
-                   onClick={() => setState({toolsVisible: !state.toolsVisible})}
-                   title="Jira Monkeys"
-            />
-            <span className="toggle-title">Jira-Monkeys</span>
-        </label>
-        {/*// TODO: marmer 05.09.2019 this should possibly be a more general container with the possible tools instead of the tools itself*/}
-        {state.toolsVisible && <WorklogSummarizerView/>}
+        <input id="JiraMonkeyToggle" type="checkbox"
+               checked={state.toolsVisible}
+               onClick={() => setState({toolsVisible: !state.toolsVisible})}
+               title="Jira Monkeys"/>
+        {state.toolsVisible && <div id="JiraMonkeyContainer"><WorklogSummarizerView/></div>}
     </div>
 }
