e83388f04aa00c1f1681724c31a9f453c4e8c9b2
MarMer <1055445+marmer@users.noreply.github.com>
2019-09-06 18:28:35 +0200

    Simple Toggling mechanics implemented

--- src/ui/JiraMonkey.tsx
+++ src/ui/JiraMonkey.tsx
@@ -1,32 +1,68 @@
-import React, {useState} from 'react';
+import React, {Component, ReactNode} from 'react';
 import "./JiraMonkey.css"
 import WorklogSummarizerView from "./WorklogSummarizerView";
-// TODO: marmer 05.09.2019 make the component refresh/reload if the "site changes" (the site content gets replaced by jira)
-export default (): React.ReactElement => {
-    const [state, setState] = useState({
-        toolsVisible: false
-    });
 
-    return <div>
-        <input id="JiraMonkeyToggle" type="checkbox"
-               checked={state.toolsVisible}
-               onClick={() => setState({toolsVisible: !state.toolsVisible})}
-               title="Jira Monkeys"/>
-        {state.toolsVisible &&
-        <div id="JiraMonkeyContainer">
-            <div className="monkeyToggler">
-                <button>Worklog-summarizer</button>
-                <button>Booking-shifter</button>
-                <button>Estimation-shifter</button>
-                <button>Estimation-fFixer</button>
-            </div>
-            <div>
-                <WorklogSummarizerView/>
-            </div>
-        </div>
-        }
-    </div>
+interface JiraMonkeyState {
+    toolsVisible: boolean
+    currentView?: ReactNode
 }
+
+export interface JiraMonkeyProps {
+
+}
+
+export default class JiraMonkey extends Component<JiraMonkeyProps, JiraMonkeyState> {
+
+    constructor(props: Readonly<JiraMonkeyProps>) {
+        super(props);
+        this.state = {
+            toolsVisible: false,
+            currentView: <WorklogSummarizerView/>
+        }
+    }
+
+    render(): React.ReactElement {
+        return <div>
+            <input id="JiraMonkeyToggle" type="checkbox"
+                   checked={this.state.toolsVisible}
+                   onClick={() => this.setState({toolsVisible: !this.state.toolsVisible})}
+                   title="Jira Monkeys"/>
+            {this.state.toolsVisible &&
+            <div id="JiraMonkeyContainer">
+                <div className="monkeyToggler">
+                    <button onClick={() => this.setCurrentView(<WorklogSummarizerView/>)}>
+                        Worklog-summarizer
+                    </button>
+                    <button onClick={() => this.setCurrentView(<div>nothing to see here yet
+                        (Booking-shifter)</div>)}>Booking-shifter
+                    </button>
+                    <button onClick={() => this.setCurrentView(<div>nothing to see here yet
+                        (Estimation-shifter)</div>)}>Estimation-shifter
+                    </button>
+                    <button onClick={() => this.setCurrentView(<div>nothing to see here yet
+                        (Estimation-fixer)</div>)}>Estimation-fixer
+                    </button>
+                </div>
+                <div>
+                    {this.state.currentView}
+                </div>
+            </div>
+            }
+        </div>
+    }
+
+    private setCurrentView(view: ReactNode) {
+        this.setState({
+            currentView: null
+        });
+        this.setState({
+            currentView: view
+        });
+    }
+}
+
+
+// TODO: marmer 05.09.2019 make the component refresh/reload if the "site changes" (the site content gets replaced by jira)
 
 
 // #### update ticket estimations
