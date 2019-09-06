2ddf039c9bcf4e0fe6df7009df22f14593489e58
MarMer <1055445+marmer@users.noreply.github.com>
2019-09-06 10:43:49 +0200

    - Ability to sort added - some rudimentary ui improvements

--- dist/jira-monkeys.meta.js
+++ dist/jira-monkeys.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          jira-monkeys
-// @version       1.0.1
+// @version       1.1.0
 // @author        MarMer
 // @description   A little script to help working with jira
 // @homepage      https://github.com/marmer/jira-monkeys
