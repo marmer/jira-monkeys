fdd7ca2084badecbb99e8df0f9c6b3ea87c62b5d
MarMer <1055445+marmer@users.noreply.github.com>
2019-09-07 14:16:17 +0200

    Some more layouting

--- src/ui/EstimationShiftView.tsx
+++ src/ui/EstimationShiftView.tsx
@@ -35,34 +35,28 @@ export default class EstimationShiftView extends Component<EstimationShiftViewPr
 
     render(): React.ReactElement {
         return <div className={"container"}>
-            {
-                this.state.sourceLoadingState === "DONE" &&
-                this.state.sourceEstimation &&
-                <EstimationView estimation={this.state.sourceEstimation} title="Source"/>}
-            {
-                this.state.sourceLoadingState === "DONE" &&
-                this.state.sourceEstimation &&
-                <EstimationView estimation={this.state.sourceEstimation} title="Destination"/>}
-            {
-                this.state.sourceLoadingState === "DONE" &&
-                this.state.sourceEstimation &&
-                <EstimationView estimation={this.state.sourceEstimation} title="Result"/>}
 
+            {this.state.sourceLoadingState === "DONE" &&
+                this.state.sourceEstimation &&
+            <EstimationView estimation={this.state.sourceEstimation}/>}
+
+            {this.state.sourceLoadingState === "DONE" &&
+                this.state.sourceEstimation &&
+            <EstimationView estimation={this.state.sourceEstimation}/>}
         </div>;
     }
 }
 
-const EstimationView = (props: { estimation: Estimation, title: string }) => {
-    return <div title={props.title} className="estimationView">
-        <h2>{props.title}</h2>
-        <h3>{props.estimation.issueKey}</h3>
+const EstimationView = (props: { estimation: Estimation }) => {
+    return <div className="estimationView">
+        <h3>
+            {props.estimation.issueKey}
+        </h3>
         <label>
-            Original Estimate:
-            <div>{props.estimation.originalEstimate}</div>
+            Original Estimate: <span>{props.estimation.originalEstimate}</span>
         </label>
         <label>
-            Remaining Estimate:
-            <div>{props.estimation.remainingEstimate}</div>
+            Remaining Estimate: <span>{props.estimation.remainingEstimate}</span>
         </label>
     </div>
 };
