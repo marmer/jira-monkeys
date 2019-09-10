480fa687e6a99c9532209f0f2f784c831c0b3cc9
MarMer <1055445+marmer@users.noreply.github.com>
2019-09-11 00:10:27 +0200

    new teststub including a failing test ;)

---
+++ src/core/IssueSiteInfos.test.ts
@@ -0,0 +1,17 @@
+import IssueSiteInfos from "./IssueSiteInfos";
+
+describe("IssueSiteInfos", () => {
+    describe("getCurrentIssueKey()", () => {
+        let oldPathName: string;
+        it('should extract an existing issue key', () => {
+
+            // TODO: marmer 11.09.2019 Time for another abstraction ;)
+
+            window.location.pathname = "/browse/MyIssueKey-1234"
+            expect(IssueSiteInfos.getCurrentIssueKey()).toBe("MyIssueKey-1234");
+
+        });
+        // TODO: marmer 10.09.2019 what about additional path parts? e.g. /browse/ISSUE-1234/blubba?
+        // TODO: marmer 10.09.2019 nonexisting
+    });
+});
