2961742c49080402655a0113f8d03c775f9dde6b
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2019-09-10 18:54:37 +0200

    test cleanup

--- src/core/EstimationService.test.ts
+++ src/core/EstimationService.test.ts
@@ -3,63 +3,65 @@ import EstimationService from "./EstimationService";
 import fetchMock from "fetch-mock"
 import deepEqual from "deep-equal";
 
-describe("something", () => {
+describe("EstimationService", () => {
     beforeEach(() => {
         fetchMock.restore()
     });
+    describe("getEstimationsForIssue", () => {
 
-    it('should serve an appropriate errormessage on a bad status', () => {
-        fetchMock.mock('http://localhost/rest/api/2/issue/issue-200', {
-            status: 500,
-            body: JSON.stringify({
-                    "fields": {
-                        "summary": "issueSummary",
-                        "timetracking": {
-                            "originalEstimate": "originalEstimate",
-                            "remainingEstimate": "remainingEstimate",
-                            "originalEstimateSeconds": 42,
-                            "remainingEstimateSeconds": 24
+        it('should serve an appropriate errormessage on a bad status', () => {
+            fetchMock.mock('http://localhost/rest/api/2/issue/issue-200', {
+                status: 500,
+                body: JSON.stringify({
+                        "fields": {
+                            "summary": "issueSummary",
+                            "timetracking": {
+                                "originalEstimate": "originalEstimate",
+                                "remainingEstimate": "remainingEstimate",
+                                "originalEstimateSeconds": 42,
+                                "remainingEstimateSeconds": 24
+                            }
                         }
                     }
-                }
-            )
+                )
+            });
+
+            EstimationService.getEstimationsForIssue("issue-200")
+                .then(result =>
+                    fail("No result should be served if no success status is served but was: " + JSON.stringify(result)))
+                .catch(error => expect(error).toBe("Unexpected request status: 500"))
         });
+        it('should return the estimations for from the rest api', () => {
 
-        EstimationService.getEstimationsForIssue("issue-200")
-            .then(result =>
-                fail("No result should be served if no success status is served"))
-            .then(error => expect(error).toBe("Unexpected request status: 500"))
-    });
-    it('should return the estimations for from the rest api', () => {
-
-        fetchMock.mock('http://localhost/rest/api/2/issue/issue-200', {
-            status: 200,
-            body: JSON.stringify({
-                    "fields": {
-                        "summary": "issueSummary",
-                        "timetracking": {
-                            "originalEstimate": "originalEstimate",
-                            "remainingEstimate": "remainingEstimate",
-                            "originalEstimateSeconds": 42,
-                            "remainingEstimateSeconds": 24
+            fetchMock.mock('http://localhost/rest/api/2/issue/issue-200', {
+                status: 200,
+                body: JSON.stringify({
+                        "fields": {
+                            "summary": "issueSummary",
+                            "timetracking": {
+                                "originalEstimate": "originalEstimate",
+                                "remainingEstimate": "remainingEstimate",
+                                "originalEstimateSeconds": 42,
+                                "remainingEstimateSeconds": 24
+                            }
                         }
                     }
-                }
-            )
-        });
+                )
+            });
 
-        EstimationService.getEstimationsForIssue("issue-200")
-            .then(result =>
-                deepEqual(result,
-                    {
-                        issueKey: "issue-200",
-                        issueSummary: "issueSummary",
-                        originalEstimate: "originalEstimate",
-                        originalEstimateInMinutes: 42,
-                        remainingEstimate: "remainingEstimate",
-                        remainingEstimateInMinutes: 24
-                    }
-                ))
-            .catch(reason => fail(reason))
+            EstimationService.getEstimationsForIssue("issue-200")
+                .then(result =>
+                    deepEqual(result,
+                        {
+                            issueKey: "issue-200",
+                            issueSummary: "issueSummary",
+                            originalEstimate: "originalEstimate",
+                            originalEstimateInMinutes: 42,
+                            remainingEstimate: "remainingEstimate",
+                            remainingEstimateInMinutes: 24
+                        }
+                    ))
+                .catch(reason => fail(reason))
+        });
     });
 });
