c31d661a354a880682cac7f4304b2fd31441fc38
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2019-09-12 14:26:10 +0200

    - less cleanup

--- setupTests.ts
+++ setupTests.ts
@@ -1,4 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
+import * as reactTest from "@testing-library/react";
 import {configure} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import fetchMock from "fetch-mock";
@@ -6,3 +7,4 @@ import fetchMock from "fetch-mock";
 configure({adapter: new Adapter()});
 
 fetchMock.restore();
+reactTest.cleanup();
