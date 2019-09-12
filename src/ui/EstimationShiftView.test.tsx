35eea11d040c1046a6f5249033edd12055c048b3
MarMer <1055445+marmer@users.noreply.github.com>
2019-09-12 19:01:10 +0200

    intermediage state

--- src/ui/EstimationShiftView.test.tsx
+++ src/ui/EstimationShiftView.test.tsx
@@ -1,6 +1,28 @@
+import * as reactTest from "@testing-library/react";
+import React from "react";
+import EstimationCrudService, {Estimation} from "../core/EstimationCrudService";
+import IssueSiteInfos from "../core/IssueSiteInfos";
+import EstimationShiftView from "./EstimationShiftView";
+
 describe("EstimationShiftView", () => {
 
-    it.todo("create some tests here ;)");
+    it("should render with estimation information for the current issue", () => {
+        const currentIssueKey123 = "currentIssue-123";
+
+        IssueSiteInfos.getCurrentIssueKey = jest.fn().mockReturnValue(currentIssueKey123);
+        EstimationCrudService.getEstimationsForIssueKey = jest.fn().mockImplementation((paramIssueKey: string): Promise<Estimation> => {
+            if (paramIssueKey === "getCurrentIssueKey") {
+                return Promise.resolve({
+// TODO: marmer 12.09.2019 go on here ... first create this Estimation ;)
+                    issueKey: paramIssueKey,
+                } as Estimation);
+            }
+            fail("unexpected issuekeyrequest: " + paramIssueKey);
+            return Promise.reject("unexpected issuekeyrequest: " + paramIssueKey);
+        });
+        const estimationShiftView = reactTest.render(<EstimationShiftView/>);
+
+    });
 
     it.skip("should do all the todos of this body ;)", () => {
         // TODO: marmer 12.09.2019 loading of this
