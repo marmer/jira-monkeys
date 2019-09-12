543947f683e8be8758062b6a4d10ea61841eee9d
MarMer <1055445+marmer@users.noreply.github.com>
2019-09-12 16:41:23 +0200

    - testpreparation for the estimations shifts

---
+++ src/ui/EstimationShiftView.test.tsx
@@ -0,0 +1,14 @@
+describe("EstimationShiftView", () => {
+
+    it.todo("create some tests here ;)");
+
+    it.skip("should do all the todos of this body ;)", () => {
+        // TODO: marmer 12.09.2019 loading of this
+        // TODO: marmer 12.09.2019 loading of others
+        // TODO: marmer 12.09.2019 handling of error when loading this
+        // TODO: marmer 12.09.2019 validation
+        // TODO: marmer 12.09.2019 sending
+        // TODO: marmer 12.09.2019 errorhandling when sending
+        // TODO: marmer 12.09.2019 local storage
+    });
+});
