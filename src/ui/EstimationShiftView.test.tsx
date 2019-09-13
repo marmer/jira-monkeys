d62c46826aeba0d3203aed52721127b12014ae9f
marmer <mariano.mertinat@gmail.com>
2019-09-13 10:04:02 +0200

    - test for loading target issue

--- src/ui/EstimationShiftView.test.tsx
+++ src/ui/EstimationShiftView.test.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import EstimationCrudService, {Estimation} from "../core/EstimationCrudService";
 import IssueSiteInfos from "../core/IssueSiteInfos";
 import EstimationShiftView from "./EstimationShiftView";
+import userEvent from "@testing-library/user-event";
 
 describe("EstimationShiftView", () => {
     const baseEstimation: Estimation = {
@@ -24,7 +25,7 @@ describe("EstimationShiftView", () => {
                 return Promise.resolve({
                     ...baseEstimation,
                     issueKey: paramIssueKey,
-                    issueSummary: "sourceSummary",
+                    issueSummary: "currentSummary",
                 });
             }
             fail("unexpected issuekeyrequest: " + paramIssueKey);
@@ -33,8 +34,7 @@ describe("EstimationShiftView", () => {
 
         const estimationShiftView = reactTest.render(<EstimationShiftView/>);
 
-        const sourceIssueView = await reactTest.waitForElement(() => estimationShiftView.getByTitle("currentIssue-123: sourceSummary"));
-        console.log(reactTest.prettyDOM(estimationShiftView.container));
+        const sourceIssueView = await reactTest.waitForElement(() => estimationShiftView.getByTitle("currentIssue-123: currentSummary"));
 
         const sourceIssueField = reactTest.getByLabelText(sourceIssueView, "Issue");
         expect(sourceIssueField).toHaveValue(currentIssueKey);
@@ -52,10 +52,71 @@ describe("EstimationShiftView", () => {
         expect(sourceRemainingEstimateField).toHaveAttribute("type", "text");
     });
 
+    it("should try to load the target issue delayed", async () => {
+        const currentEstimation = {
+            ...baseEstimation,
+            issueKey: "currentIssue-123",
+            issueSummary: "currentSummary",
+        };
+        const targetEstimation = {
+            ...baseEstimation,
+            issueKey: "targetIssue-123",
+            issueSummary: "targetSummary",
+            remainingEstimate: "1w",
+            originalEstimate: "2w",
+        };
+
+
+        IssueSiteInfos.getCurrentIssueKey = jest.fn().mockReturnValue(currentEstimation.issueKey);
+
+        EstimationCrudService.getEstimationsForIssueKey = jest.fn().mockImplementation((paramIssueKey: string): Promise<Estimation> => {
+            if (paramIssueKey === targetEstimation.issueKey) {
+                return Promise.resolve(targetEstimation);
+            }
+
+            if (paramIssueKey === currentEstimation.issueKey) {
+                return Promise.resolve(currentEstimation);
+            }
+
+            fail("No request expected for an issue with key: " + paramIssueKey);
+            return Promise.reject("No request expected for an issue with key: " + paramIssueKey);
+        });
+
+        const estimationShiftView = reactTest.render(<EstimationShiftView/>);
+
+        const estimationShiftInput = estimationShiftView.getByLabelText("Time to shift");
+        expect(estimationShiftInput).toBeDisabled();
+
+        const targetIssueKeyInput = estimationShiftView.getByLabelText("Issue key");
+        await userEvent.type(targetIssueKeyInput, "unfinishedInput");
+        expect(estimationShiftInput).toBeDisabled();
+        await userEvent.type(targetIssueKeyInput, targetEstimation.issueKey);
+        expect(estimationShiftInput).toBeDisabled();
+
+        expect(estimationShiftView.queryByTitle(labelText => labelText.endsWith(targetEstimation.issueSummary))).toBeNull();
+        const targetIssueView = await reactTest.waitForElement(() => estimationShiftView.getByTitle(labelText => labelText.endsWith(targetEstimation.issueKey + ": " + targetEstimation.issueSummary)));
+
+        expect(estimationShiftInput).toBeEnabled();
+
+        const sourceIssueField = reactTest.getByLabelText(targetIssueView, "Issue");
+        expect(sourceIssueField).toHaveValue(targetEstimation.issueKey);
+        expect(sourceIssueField).toBeDisabled();
+        expect(sourceIssueField).toHaveAttribute("type", "text");
+
+        const sourceOriginalEstimateField = reactTest.getByLabelText(targetIssueView, "Original Estimate");
+        expect(sourceOriginalEstimateField).toHaveValue(targetEstimation.originalEstimate);
+        expect(sourceOriginalEstimateField).toBeDisabled();
+        expect(sourceOriginalEstimateField).toHaveAttribute("type", "text");
+
+        const sourceRemainingEstimateField = reactTest.getByLabelText(targetIssueView, "Remaining Estimate");
+        expect(sourceRemainingEstimateField).toHaveValue(targetEstimation.remainingEstimate);
+        expect(sourceRemainingEstimateField).toBeDisabled();
+        expect(sourceRemainingEstimateField).toHaveAttribute("type", "text");
+    });
+
     it.skip("should do all the todos of this body ;)", () => {
-        // TODO: marmer 12.09.2019 loading of this
-        // TODO: marmer 12.09.2019 loading of others
-        // TODO: marmer 12.09.2019 handling of error when loading this
+        // TODO: marmer 12.09.2019 handling of error when loading THIS
+        // TODO: marmer 12.09.2019 handling of error when loading target
         // TODO: marmer 12.09.2019 validation
         // TODO: marmer 12.09.2019 sending
         // TODO: marmer 12.09.2019 errorhandling when sending
