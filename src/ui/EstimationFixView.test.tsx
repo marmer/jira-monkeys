7809acaf819bbf5237b2e9fc21668335c048e2f1
MarMer <1055445+marmer@users.noreply.github.com>
2019-09-26 12:18:38 +0200

    - Errorhandling for the estimation fix view

--- src/ui/EstimationFixView.test.tsx
+++ src/ui/EstimationFixView.test.tsx
@@ -1,10 +1,18 @@
 import * as reactTest from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import React from "react";
+import React, {FunctionComponent} from "react";
 import EstimationFixService from "../core/EstimationFixService";
 import IssueSiteInfos from "../core/IssueSiteInfos";
 import WindowService from "../core/WindowService";
 import EstimationFixView from "./EstimationFixView";
+
+jest.mock("./ModalView", () => ((({children, onClose}) =>
+    <>
+        <div data-testid="errorViewMock">
+            {children}
+        </div>
+        <button data-testid="errorViewCloseButton" onClick={onClose}>X</button>
+    </>) as FunctionComponent<{ onClose: () => void }>));
 
 describe("EstimationFixView", () => {
     it("should fixing the estimation should reload the page", async () => {
@@ -12,11 +20,10 @@ describe("EstimationFixView", () => {
 
         IssueSiteInfos.getCurrentIssueKey = jest.fn().mockReturnValue(currentIssueKey);
         EstimationFixService.fixEstimationForIssue = jest.fn().mockImplementation(issueKey => {
-            if (issueKey === currentIssueKey) {
-                return Promise.resolve();
+            if (issueKey !== currentIssueKey) {
+                fail("unexpected issue key request: " + issueKey);
             }
-            fail("unexpected issue key request: " + issueKey);
-            return Promise.reject("Should not be reachable ");
+            return Promise.resolve();
         });
 
         WindowService.reloadPage = jest.fn();
@@ -29,7 +36,28 @@ describe("EstimationFixView", () => {
         expect(WindowService.reloadPage).toBeCalled();
     });
 
-    // TODO: marmer 26.09.2019 handle error when estimation fix does not work
+    it("should show an error message without reloading", async () => {
+        const currentIssueKey = "CURRENT-1234";
 
-    // TODO: marmer 26.09.2019 Give a preview for the fixed estimation
+        IssueSiteInfos.getCurrentIssueKey = jest.fn().mockReturnValue(currentIssueKey);
+        EstimationFixService.fixEstimationForIssue = jest.fn().mockImplementation(issueKey => {
+            if (issueKey !== currentIssueKey) {
+                fail("unexpected issue key request: " + issueKey);
+            }
+            return Promise.reject(new Error("something bad happend"));
+        });
+
+        WindowService.reloadPage = jest.fn();
+
+        const estimationFixView = reactTest.render(<EstimationFixView/>);
+
+        userEvent.click(estimationFixView.getByText("Fix estimation"));
+
+        const errorView = await reactTest.waitForElement(() => estimationFixView.getByTestId("errorViewMock"));
+        await reactTest.wait(() => expect(WindowService.reloadPage).not.toBeCalled());
+        expect(errorView).toHaveTextContent("An unexpected error occurred while fixing the estimations. The page is getting reloaded on closing this message. Check the estimation afterwards");
+        userEvent.click(estimationFixView.getByTestId("errorViewCloseButton"));
+
+        await reactTest.wait(() => expect(WindowService.reloadPage).toBeCalled());
+    });
 });
--- src/ui/EstimationFixView.tsx
+++ src/ui/EstimationFixView.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {Component} from "react";
 import EstimationFixService from "../core/EstimationFixService";
 import IssueSiteInfos from "../core/IssueSiteInfos";
 import WindowService from "../core/WindowService";
+import ModalView from "./ModalView";
 
-export default (): React.ReactElement => <div>
-    <button
-        onClick={() =>
-            EstimationFixService.fixEstimationForIssue(IssueSiteInfos.getCurrentIssueKey())
-                .then(() => WindowService.reloadPage())}>
-        Fix estimation
-    </button>
-</div>;
+interface EstimationFixViewState {
+    errorMessage?: string | null;
+}
+
+export default class EstimationFixView extends Component<{}, EstimationFixViewState> {
+
+    constructor(props: Readonly<{}>) {
+        super(props);
+        this.state = {};
+    }
+
+    public render(): React.ReactElement {
+        return <div>
+            <button
+                onClick={() =>
+                    EstimationFixService.fixEstimationForIssue(IssueSiteInfos.getCurrentIssueKey())
+                        .then(() => WindowService.reloadPage())
+                        .catch(() => this.setState({errorMessage: "An unexpected error occurred while fixing the estimations. The page is getting reloaded on closing this message. Check the estimation afterwards"}))}>
+                Fix estimation
+            </button>
+            {this.state.errorMessage && <ModalView onClose={WindowService.reloadPage}>
+                {this.state.errorMessage}
+            </ModalView>}
+        </div>;
+    }
+
+}
