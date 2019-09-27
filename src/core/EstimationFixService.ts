cda05fb59fcfd57c3d3847d81c94f6663b60b923
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2019-09-27 18:11:21 +0200

    - a little refactoring

--- src/core/EstimationFixService.ts
+++ src/core/EstimationFixService.ts
@@ -1,4 +1,4 @@
-import EstimationCrudService from "./EstimationCrudService";
+import EstimationCrudService, {Estimation} from "./EstimationCrudService";
 import JiraTimeService from "./JiraTimeService";
 
 export default class EstimationFixService {
@@ -7,31 +7,31 @@ export default class EstimationFixService {
             throw new Error("Error while loading the estimation to fix");
         });
 
-        const {originalEstimateInMinutes, timeSpentMinutes} = estimation;
+        const fixedRemainingEstimation = this.calculateRemainingEstimation(estimation);
 
-        let remainingEstimateInMinutes: number;
-        if (originalEstimateInMinutes) {
-            remainingEstimateInMinutes = originalEstimateInMinutes -
-                (timeSpentMinutes ? timeSpentMinutes : 0);
-            if (remainingEstimateInMinutes < 0) {
-                remainingEstimateInMinutes = 0;
-            }
-        } else {
-            remainingEstimateInMinutes = 0;
-        }
-
-        if (remainingEstimateInMinutes === (estimation.remainingEstimateInMinutes ? estimation.remainingEstimateInMinutes : 0)) {
+        if (fixedRemainingEstimation === (estimation.remainingEstimateInMinutes ? estimation.remainingEstimateInMinutes : 0)) {
             return;
         }
 
         try {
             await EstimationCrudService.updateEstimation({
                 ...estimation,
-                remainingEstimateInMinutes,
-                remainingEstimate: JiraTimeService.minutesToJiraFormat(remainingEstimateInMinutes),
+                remainingEstimateInMinutes: fixedRemainingEstimation,
+                remainingEstimate: JiraTimeService.minutesToJiraFormat(fixedRemainingEstimation),
             });
         } catch (e) {
             throw new Error("Error while updating the estimation to fix");
         }
+    }
+
+    private static calculateRemainingEstimation(estimation: Estimation) {
+        const {originalEstimateInMinutes, timeSpentMinutes} = estimation;
+
+        if (!originalEstimateInMinutes) {
+            return 0;
+        }
+
+        const remainingEstimateInMinutes: number = originalEstimateInMinutes - (timeSpentMinutes ? timeSpentMinutes : 0);
+        return remainingEstimateInMinutes < 0 ? 0 : remainingEstimateInMinutes;
     }
 }
