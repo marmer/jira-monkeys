879d9f25ffa9e19e28c54697efd673369fd75c6a
MarMer <1055445+marmer@users.noreply.github.com>
2019-09-27 09:07:04 +0200

    - a little test refactoring

--- src/core/EstimationFixService.test.ts
+++ src/core/EstimationFixService.test.ts
@@ -14,41 +14,47 @@ describe("EstimationFixService", () => {
             issueKey: "ISSUE-1234",
         };
 
-        it("should shift estimations correctly", async () => {
-            const estimationToFix: Estimation = {
-                ...baseEstimation,
-                originalEstimate: "10m",
-                originalEstimateInMinutes: 10,
-                remainingEstimate: "5m",
-                remainingEstimateInMinutes: 5,
-                timeSpent: "8m",
-                timeSpentMinutes: 8,
-            };
-
-            const fixedEstimation: Estimation = {
-                ...baseEstimation,
-                originalEstimate: "10m",
-                originalEstimateInMinutes: 10,
-                remainingEstimate: minutesToJiraFormat(2),
-                remainingEstimateInMinutes: 2,
-                timeSpent: "8m",
-                timeSpentMinutes: 8,
-            };
-
-            EstimationCrudService.getEstimationsForIssueKey = jest.fn().mockImplementation(
-                async issueKey => {
-                    if (issueKey !== estimationToFix.issueKey) {
-                        fail("Unexpected issue to load: " + issueKey);
-                    }
-                    return estimationToFix;
+        ([
+            {
+                estimationToFix: {
+                    ...baseEstimation,
+                    originalEstimate: "10m",
+                    originalEstimateInMinutes: 10,
+                    remainingEstimate: "5m",
+                    remainingEstimateInMinutes: 5,
+                    timeSpent: "8m",
+                    timeSpentMinutes: 8,
                 },
-            );
-            EstimationCrudService.updateEstimation = jest.fn();
-            await EstimationFixService.fixEstimationForIssue(estimationToFix.issueKey);
+                fixedEstimation: {
+                    ...baseEstimation,
+                    originalEstimate: "10m",
+                    originalEstimateInMinutes: 10,
+                    remainingEstimate: minutesToJiraFormat(2),
+                    remainingEstimateInMinutes: 2,
+                    timeSpent: "8m",
+                    timeSpentMinutes: 8,
+                },
+            },
+        ] as [{ estimationToFix: Estimation, fixedEstimation: Estimation }])
+            .map(({estimationToFix, fixedEstimation}) => {
+                it("should shift estimations for " + estimationToFix + " to " + fixedEstimation, async () => {
+                    EstimationCrudService.getEstimationsForIssueKey = jest.fn().mockImplementation(
+                        async issueKey => {
+                            if (issueKey !== estimationToFix.issueKey) {
+                                fail("Unexpected issue to load: " + issueKey);
+                            }
+                            return estimationToFix;
+                        },
+                    );
+                    EstimationCrudService.updateEstimation = jest.fn();
+                    await EstimationFixService.fixEstimationForIssue(estimationToFix.issueKey);
 
-            expect(EstimationCrudService.updateEstimation).toBeCalledWith(fixedEstimation);
+                    expect(EstimationCrudService.updateEstimation).toBeCalledWith(fixedEstimation);
 
-            // TODO: marmer 26.09.2019 go on here with a "few" more possibilities
-        });
+                    // TODO: marmer 26.09.2019 go on here with a "few" more possibilities
+                });
+            });
+        // TODO: marmer 27.09.2019 add negative cases
+        // TODO: marmer 27.09.2019 add tests with nothing todo
     });
 });
