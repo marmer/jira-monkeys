2d058cefc7cf1ba13e1639231e9dd71e3790d885
MarMer <1055445+marmer@users.noreply.github.com>
2019-09-27 09:28:07 +0200

    - positive tests with the need to fix something added

--- src/core/EstimationFixService.test.ts
+++ src/core/EstimationFixService.test.ts
@@ -16,11 +16,12 @@ describe("EstimationFixService", () => {
 
         ([
             {
+                description: "too much remaining",
                 estimationToFix: {
                     ...baseEstimation,
                     originalEstimate: "10m",
                     originalEstimateInMinutes: 10,
-                    remainingEstimate: "5m",
+                    remainingEstimate: minutesToJiraFormat(5),
                     remainingEstimateInMinutes: 5,
                     timeSpent: "8m",
                     timeSpentMinutes: 8,
@@ -34,10 +35,90 @@ describe("EstimationFixService", () => {
                     timeSpent: "8m",
                     timeSpentMinutes: 8,
                 },
+            }, {
+                description: "not enough remaining",
+                estimationToFix: {
+                    ...baseEstimation,
+                    originalEstimate: "10m",
+                    originalEstimateInMinutes: 10,
+                    remainingEstimate: minutesToJiraFormat(5),
+                    remainingEstimateInMinutes: 5,
+                    timeSpent: "2m",
+                    timeSpentMinutes: 2,
+                },
+                fixedEstimation: {
+                    ...baseEstimation,
+                    originalEstimate: "10m",
+                    originalEstimateInMinutes: 10,
+                    remainingEstimate: minutesToJiraFormat(8),
+                    remainingEstimateInMinutes: 8,
+                    timeSpent: "2m",
+                    timeSpentMinutes: 2,
+                },
+            }, {
+                description: "undefined remaining",
+                estimationToFix: {
+                    ...baseEstimation,
+                    originalEstimate: "10m",
+                    originalEstimateInMinutes: 10,
+                    remainingEstimate: undefined,
+                    remainingEstimateInMinutes: undefined,
+                    timeSpent: "2m",
+                    timeSpentMinutes: 2,
+                },
+                fixedEstimation: {
+                    ...baseEstimation,
+                    originalEstimate: "10m",
+                    originalEstimateInMinutes: 10,
+                    remainingEstimate: minutesToJiraFormat(8),
+                    remainingEstimateInMinutes: 8,
+                    timeSpent: "2m",
+                    timeSpentMinutes: 2,
+                },
+            }, {
+                description: "undefined timespent",
+                estimationToFix: {
+                    ...baseEstimation,
+                    originalEstimate: "10m",
+                    originalEstimateInMinutes: 10,
+                    remainingEstimate: minutesToJiraFormat(3),
+                    remainingEstimateInMinutes: 3,
+                    timeSpent: undefined,
+                    timeSpentMinutes: undefined,
+                },
+                fixedEstimation: {
+                    ...baseEstimation,
+                    originalEstimate: "10m",
+                    originalEstimateInMinutes: 10,
+                    remainingEstimate: minutesToJiraFormat(10),
+                    remainingEstimateInMinutes: 10,
+                    timeSpent: undefined,
+                    timeSpentMinutes: undefined,
+                },
+            }, {
+                description: "undefined original",
+                estimationToFix: {
+                    ...baseEstimation,
+                    originalEstimate: undefined,
+                    originalEstimateInMinutes: undefined,
+                    remainingEstimate: minutesToJiraFormat(3),
+                    remainingEstimateInMinutes: 3,
+                    timeSpent: minutesToJiraFormat(2),
+                    timeSpentMinutes: 2,
+                },
+                fixedEstimation: {
+                    ...baseEstimation,
+                    originalEstimate: undefined,
+                    originalEstimateInMinutes: undefined,
+                    remainingEstimate: minutesToJiraFormat(0),
+                    remainingEstimateInMinutes: 0,
+                    timeSpent: minutesToJiraFormat(2),
+                    timeSpentMinutes: 2,
+                },
             },
-        ] as [{ estimationToFix: Estimation, fixedEstimation: Estimation }])
-            .map(({estimationToFix, fixedEstimation}) => {
-                it("should shift estimations for " + estimationToFix + " to " + fixedEstimation, async () => {
+        ] as Array<{ description: string, estimationToFix: Estimation, fixedEstimation: Estimation }>)
+            .map(({description, estimationToFix, fixedEstimation}) => {
+                it("should shift estimation when there is " + description, async () => {
                     EstimationCrudService.getEstimationsForIssueKey = jest.fn().mockImplementation(
                         async issueKey => {
                             if (issueKey !== estimationToFix.issueKey) {
@@ -50,11 +131,10 @@ describe("EstimationFixService", () => {
                     await EstimationFixService.fixEstimationForIssue(estimationToFix.issueKey);
 
                     expect(EstimationCrudService.updateEstimation).toBeCalledWith(fixedEstimation);
-
-                    // TODO: marmer 26.09.2019 go on here with a "few" more possibilities
                 });
             });
-        // TODO: marmer 27.09.2019 add negative cases
+        // TODO: marmer 27.09.2019 add negative cases (not able to load)
+        // TODO: marmer 27.09.2019 add negative cases (not able to write)
         // TODO: marmer 27.09.2019 add tests with nothing todo
     });
 });
