82fa66dc01233859c99b781a45b4129eecfbec34
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2019-10-07 18:44:35 +0200

    work in Progress

--- src/core/WorklogShiftService.ts
+++ src/core/WorklogShiftService.ts
@@ -1,6 +1,7 @@
 export default class WorklogShiftService {
     public static async shiftFromWorklog(): Promise<void> {
 
+        // TODO: marmer 07.10.2019 Implement me!
         return Promise.reject(new Error("Not implemented yet"));
     }
 }
