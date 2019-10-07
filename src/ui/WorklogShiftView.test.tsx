9f006720bb71c8c59a4d9fc31d64fa77fcaabb6c
MarMer <1055445+marmer@users.noreply.github.com>
2019-10-07 14:01:15 +0200

    - Displaying of own worklogs for the current issue

---
+++ src/ui/WorklogShiftView.test.tsx
@@ -0,0 +1,51 @@
+import * as reactTest from "@testing-library/react";
+import React from "react";
+import WorklogService, {Worklog} from "../core/WorklogService";
+import WorklogShiftView from "./WorklogShiftView";
+
+describe("WorklogShiftView", () => {
+
+    const worklogBase: Worklog = {
+        issueId: "42",
+        author: {
+            name: "real.me",
+            displayName: "Me",
+        },
+        timeSpentInMinutes: 1337,
+        started: "yesterday",
+        id: "43",
+        comment: "did something",
+    };
+
+    it("should show the estimations for the current user and issue when loading is done", async () => {
+        WorklogService.getWorklogsForCurrentIssueAndUser = jest.fn().mockResolvedValue([
+            {
+                ...worklogBase,
+                author: {...worklogBase.author, displayName: "Me"},
+                comment: "first step",
+                started: "2019-10-01T01:02:03.000+0200",
+                timeSpentInMinutes: 1,
+                id: "1111",
+                issueId: "11",
+            },
+            {
+                ...worklogBase,
+                author: {...worklogBase.author, displayName: "Me Too"},
+                comment: "second step",
+                started: "2020-01-10T02:03:04.000+0200",
+                timeSpentInMinutes: 2,
+                id: "2222",
+                issueId: "22",
+            }] as Worklog[]);
+
+        const underTest = reactTest.render(<WorklogShiftView/>);
+        await reactTest.waitForElement(() => underTest.getByDisplayValue("first step"));
+        expect(underTest).toMatchSnapshot();
+    });
+
+    // TODO: marmer 07.10.2019 loading
+    // TODO: marmer 07.10.2019 errorhandling while loading
+    // TODO: marmer 07.10.2019 errorhandling while shifting
+    // TODO: marmer 07.10.2019 Shifting
+    // TODO: marmer 07.10.2019 Handling of missing worklog parts (author, Comment, Start, ...)
+});
--- src/ui/WorklogShiftView.tsx
+++ src/ui/WorklogShiftView.tsx
@@ -1,5 +1,5 @@
 import moment = require("moment");
-import React, {Component} from "react";
+import React, {Component, ReactNode} from "react";
 import JiraTimeService from "../core/JiraTimeService";
 import WorklogService, {Worklog} from "../core/WorklogService";
 import EstimationView from "./EstimationView";
@@ -24,24 +24,11 @@ export default class WorklogShiftView extends Component<{}, WorklogShiftViewStat
             .then(worklogs => this.setState({worklogs}));
     }
 
-// Worklog structure (It exists already in an incomplete state)
-//     export interface Worklog {
-//     author: {
-//         displayName: string,
-//         name: string,
-//     };
-//     timeSpentInMinutes: number;
-//     started: string ("2019-09-02T16:03:00.000+0200");
-//     id: string;
-//     comment: string;
-// }
-
     public render(): React.ReactElement {
         return <>
             {/*// TODO: marmer 30.09.2019 This is just a mockup with inline styles*/}
             {/*// TODO: marmer 30.09.2019 Don't use the layout of a different View in this way*/}
-            This is just a mockup remove this test when done!
-            {JSON.stringify(this.state.worklogs)}
+            This is just work in Progress
             <div className="estimationShiftContainer">
                 <table>
                     <thead>
@@ -55,86 +42,9 @@ export default class WorklogShiftView extends Component<{}, WorklogShiftViewStat
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value="Sir Fakeisac Newton"/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value="Did the first step"/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value={moment("2019-09-02T16:03:00.000+0200").format("YYYY-MM-DD HH:mm:ss")}/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value={JiraTimeService.minutesToJiraFormat(225)}/></td>
-                        <td><input style={{textAlign: "center"}} type="text" placeholder="5h 9m"
-                                   value={JiraTimeService.minutesToJiraFormat(225)}/>
-                        </td>
-                        <td>
-                            <button title="move">{">"}</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value="Sir Fakeisac Newton"/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value="Did the second step"/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value={moment("2019-09-02T17:03:00.000+0200").format("YYYY-MM-DD HH:mm:ss")}/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value={JiraTimeService.minutesToJiraFormat(135)}/></td>
-                        <td><input style={{textAlign: "center"}} type="text" placeholder="5h 9m"
-                                   value={JiraTimeService.minutesToJiraFormat(135)}/>
-                        </td>
-                        <td>
-                            <button title="move">{">"}</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value="Sir Fakeisac Newton"/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value="Did nothing"/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value={moment("2019-09-02T17:05:00.000+0200").format("YYYY-MM-DD HH:mm:ss")}/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value={JiraTimeService.minutesToJiraFormat(240)}/></td>
-                        <td><input style={{textAlign: "center"}} type="text" placeholder="5h 9m"
-                                   value={JiraTimeService.minutesToJiraFormat(240)}/>
-                        </td>
-                        <td>
-                            <button title="move">{">"}</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value="Sir Fakeisac Newton"/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value="Did the third step"/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value={moment("2019-09-09T17:05:00.000+0200").format("YYYY-MM-DD HH:mm:ss")}/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value={JiraTimeService.minutesToJiraFormat(1337)}/></td>
-                        <td><input style={{textAlign: "center"}} type="text" placeholder="5h 9m"
-                                   value={JiraTimeService.minutesToJiraFormat(1337)}/>
-                        </td>
-                        <td>
-                            <button title="move">{">"}</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value="Sir Fakeisac Newton"/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value="Did the final step"/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value={moment("2019-11-12T17:05:00.000+0200").format("YYYY-MM-DD HH:mm:ss")}/></td>
-                        <td><input style={{textAlign: "center"}} type="text" disabled={true}
-                                   value={JiraTimeService.minutesToJiraFormat(123)}/></td>
-                        <td><input style={{textAlign: "center"}} type="text" placeholder="5h 9m"
-                                   value={JiraTimeService.minutesToJiraFormat(123)}/>
-                        </td>
-                        <td>
-                            <button title="move">{">"}</button>
-                        </td>
-                    </tr>
+                    {
+                        this.state.worklogs.map(this.toRow)
+                    }
                     </tbody>
                 </table>
                 <label style={{textAlign: "center", alignSelf: "baseline"}}>
@@ -151,4 +61,23 @@ export default class WorklogShiftView extends Component<{}, WorklogShiftViewStat
         </>;
     }
 
+    private toRow(worklog: Worklog): ReactNode {
+
+        return <tr key={worklog.id}>
+            <td><input style={{textAlign: "center"}} type="text" disabled={true}
+                       value={worklog.author.displayName}/></td>
+            <td><input style={{textAlign: "center"}} type="text" disabled={true}
+                       value={worklog.comment}/></td>
+            <td><input style={{textAlign: "center"}} type="text" disabled={true}
+                       value={moment(worklog.started).format("YYYY-MM-DD HH:mm:ss")}/></td>
+            <td><input style={{textAlign: "center"}} type="text" disabled={true}
+                       value={JiraTimeService.minutesToJiraFormat(worklog.timeSpentInMinutes)}/></td>
+            <td><input style={{textAlign: "center"}} type="text" placeholder="5h 9m"
+                       value={JiraTimeService.minutesToJiraFormat(worklog.timeSpentInMinutes)}/>
+            </td>
+            <td>
+                <button title="move">{">"}</button>
+            </td>
+        </tr>;
+    }
 }
