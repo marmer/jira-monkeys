7c80bc5d32307c462f67f1c1e3293b6d874de4fa
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2019-10-08 15:43:42 +0200

    - default timezone for tests is Europe/Berlin now

--- setupTests.ts
+++ setupTests.ts
@@ -3,8 +3,11 @@ import * as reactTest from "@testing-library/react";
 import {configure} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import fetchMock from "fetch-mock";
+import moment = require("moment-timezone");
 
 configure({adapter: new Adapter()});
 
 fetchMock.restore();
 reactTest.cleanup();
+
+moment.tz.setDefault("Europe/Berlin");
