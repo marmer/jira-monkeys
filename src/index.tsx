fd01ec329b73845859001f119b2d1a128a60e993
MarMer <1055445+marmer@users.noreply.github.com>
2019-10-13 10:48:24 +0200

    - intermediate state. Try to get hidden reloads

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,37 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import JiraMonkey from "./ui/JiraMonkey";
 
+import * as packageJson from '../package.json';
+
 // tslint:disable-next-line:only-arrow-functions
 (function() {
     // if no body is here yet, it is (currently) ok when the application crashes
     const body = document.querySelectorAll("body")[0];
     const appContainer = document.createElement("nav");
     body.prepend(appContainer);
-    ReactDOM.render(<JiraMonkey/>, appContainer);
+
+    let lastJiraMonkey: any = null;
+    let lastLocation = window.location.href;
+
+
+    const tryMount = () => {
+        // TODO: marmer 13.10.2019 maybe better match to "window.location.pathname? ;)
+        if (window.location.href.startsWith(packageJson.tampermonkey.match.baseUrl + "/browse/")) {
+            lastJiraMonkey = <JiraMonkey/>;
+            ReactDOM.render(lastJiraMonkey, appContainer);
+        }
+    };
+
+    tryMount();
+
+    const interval = setInterval(() => {
+        if (lastLocation !== window.location.href) {
+            lastLocation = window.location.href;
+            if (lastJiraMonkey) {
+                ReactDOM.unmountComponentAtNode(appContainer);
+                lastJiraMonkey = null;
+            }
+            tryMount();
+        }
+    }, 100)
 })();
