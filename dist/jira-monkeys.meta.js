00b6a338d73488b844c538819df8c2e184f07e22
marmer <mariano.mertinat@gmail.com>
2019-10-13 13:32:00 +0200

    - Jira Monkey position changed to get rid of some chrome behavior on a few machines when someone scrolls in jira

--- dist/jira-monkeys.meta.js
+++ dist/jira-monkeys.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name jira-monkeys
-// @version 1.8.0
+// @version 1.9.0
 // @author MarMer
 // @description A little script to help working with jira
 // @homepage https://github.com/marmer/jira-monkeys
