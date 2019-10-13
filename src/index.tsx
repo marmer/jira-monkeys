25cd43063418852310e555d48bf5ca2910bd5cf0
MarMer <1055445+marmer@users.noreply.github.com>
2019-10-13 13:32:00 +0200

    - Jira Monkey position changed to get rid of some chrome behavior on a few machines when someone scrolls in jira

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,8 @@ import JiraMonkey from "./ui/JiraMonkey";
 // tslint:disable-next-line:only-arrow-functions
 (function() {
     // if no body is here yet, it is (currently) ok when the application crashes
-    const body = document.querySelectorAll("body")[0];
+    const content = document.getElementById("content")
+    const body = content ? content : document.querySelectorAll("body")[0];
     const appContainer = document.createElement("nav");
     body.prepend(appContainer);
 
