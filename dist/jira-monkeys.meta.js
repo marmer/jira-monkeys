cda0f9987753b46f5d4ec6b7a7c5d4942b6bb149
marmer <mariano.mertinat@gmail.com>
2019-10-13 11:25:54 +0200

    - url rewrites can cause an update without a complete site reload

--- dist/jira-monkeys.meta.js
+++ dist/jira-monkeys.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name jira-monkeys
-// @version 1.7.0
+// @version 1.8.0
 // @author MarMer
 // @description A little script to help working with jira
 // @homepage https://github.com/marmer/jira-monkeys
-// @match https://jira.schuetze.ag/browse/*
+// @match https://jira.schuetze.ag/*
 // @namespace https://github.com/marmer/jira-monkeys
 // @grant none
 // @updateURL https://github.com/marmer/jira-monkeys/raw/master/dist/jira-monkeys.user.js
