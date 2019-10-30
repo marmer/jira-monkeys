e0bbd6839141d689b203f612ae05e9fab02c36d9
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2019-10-30 18:03:02 +0100

    - Scroll bar on the right side is visible now

--- dist/jira-monkeys.meta.js
+++ dist/jira-monkeys.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name jira-monkeys
-// @version 1.11.1
+// @version 1.11.2
 // @author MarMer
 // @description A little script to help working with jira
 // @homepage https://github.com/marmer/jira-monkeys
